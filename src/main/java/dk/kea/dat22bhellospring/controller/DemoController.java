31807431e4f73d83a5b4e6ad546fbbcff03e5818
jarltuxen <jarl.tuxen@gmail.com>
2023-02-08 12:28:04 +0100

    html-fil

--- src/main/java/dk/kea/dat22bhellospring/controller/DemoController.java
+++ src/main/java/dk/kea/dat22bhellospring/controller/DemoController.java
@@ -1,6 +1,7 @@
 package dk.kea.dat22bhellospring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class DemoController {
     @GetMapping("/hejdat22b")
     public String helloDat22b(){
         return "Hej Dat22b!<br>Tid for en øvelse!";
+    }
+
+    @GetMapping("/parameter")
+    public String parameterTest(@RequestParam String input){
+        return input;
     }
 
 }
