02bb2e7e417aafdd3d0bc9236880f41753726b7f
jarltuxen <jarl.tuxen@gmail.com>
2023-02-08 10:38:32 +0100

    Initial commit

---
+++ src/main/java/dk/kea/dat22bhellospring/Dat22bhellospringApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.dat22bhellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat22bhellospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dat22bhellospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/dat22bhellospring/Dat22bhellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.dat22bhellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat22bhellospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
