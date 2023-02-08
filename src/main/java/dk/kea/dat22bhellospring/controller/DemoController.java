476170d4b9edfa22675671c9ef16ddf286cdea96
jarltuxen <jarl.tuxen@gmail.com>
2023-02-08 11:08:24 +0100

    ny gitignore

---
+++ src/main/java/dk/kea/dat22bhellospring/controller/DemoController.java
@@ -0,0 +1,14 @@
+package dk.kea.dat22bhellospring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hejdat22b")
+    public String helloDat22b(){
+        return "Hej Dat22b!<br>Tid for en øvelse!";
+    }
+
+}
